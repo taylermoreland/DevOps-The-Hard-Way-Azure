73d2fd7948e730914ef00a64dbd820a718e5b390
taylermoreland <81175752+taylermoreland@users.noreply.github.com>
2024-02-02 15:49:25 +0000

    fixes

--- Terraform-AZURE-Services-Creation/AKS/aks.tf
+++ Terraform-AZURE-Services-Creation/AKS/aks.tf
@@ -19,7 +19,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   dns_prefix          = "${var.name}dns"
   kubernetes_version  = var.kubernetes_version
 
-  node_resource_group = "${var.name}-node-rg"
+  node_resource_group = "nl004pa-tayler-dev"
 
   linux_profile {
     admin_username = "ubuntu"
