1251958a110b3825f3d46f5b0251aa1e49b79579
taylermoreland <81175752+taylermoreland@users.noreply.github.com>
2024-02-02 15:24:26 +0000

    test

--- Terraform-AZURE-Services-Creation/ACR/main.tf
+++ Terraform-AZURE-Services-Creation/ACR/main.tf
@@ -21,7 +21,7 @@ resource "azurerm_resource_group" "acr_resource_group" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "${var.name}acr"
+  name                = "nl004acrdev"
   resource_group_name = azurerm_resource_group.acr_resource_group.name
   location            = azurerm_resource_group.acr_resource_group.location
   sku                 = "Standard"
